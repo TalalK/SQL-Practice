c7166d8b97b78d113549e4dc35f166a524a3d682
Talal Khodr <Talal.Khodr@Gmail.com>
2019-11-07 19:26:13 -0800

    Chapter 7 in class
    Class 6 was postponed and merged into an online one.

---
+++ Script_4.sql
@@ -0,0 +1,27 @@
+-- previously covered, SubQueried and Joins.
+-- Subqueries join tables without any relationship with each other EG, Primary and secondary Key
+
+
+select cust_name 
+from customes
+where cust_id in 
+(
+  select DISTINCT cust_od
+  from orders
+  where order_num in 
+  (
+    Select order_num
+    from orderitems
+    where prod_id = 'BNBG03'
+  )
+):
+-- five items in an order, with two distinct orders from a customer ID, which will return only one customer.
+-- Subquery only use, was in the 18 inch teddy bear example. As Subquerying can return a price as a filter.
+-- Joining would need two tables, however the sae table may be joined twice.
+
+select cust_id, count(*) from orders group by cust_id;
+
+select * from orders where cust_id = '1000000001'
+
+
+
