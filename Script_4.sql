52d7244416f2a493b19168cca78e5ef138ba5f46
Talal Khodr <Talal.Khodr@Gmail.com>
2019-11-14 21:06:45 -0800

    Lec 8
    Added Lec 8 Material
    Covering
    Checking data
    Creating NULLABLE tables

--- Script_4.sql
+++ Script_4.sql
@@ -47,5 +47,51 @@ values  Values
   'Totally_Toys');
 
  
+ -- lecture 8
+ -- NULLability tables
+ -- PRIMARY KEYS CANNOT BE NULL
  
+ Create TABLE Vendors
+ (
+   vend_id  char(10) NOT NULL,
+   vend_name char(50) UNIQUE NOT NULL,
+   vend_name char(50) NULL,
+   vend_address char(50) NULL, 
+ 
+   CONSTRAINT PK_Products
+    PRIMARY KEY CLUSTERED(prod_id),
+   
+   CONSTRAINT PK_PRODUCTS_VENDORS
+    FOREIGN KEY(vend_id)
+      REFERENCES Vendors(vend_id)
+ );
+  
+-- Unique constraint makes sure that only one reference of the value is addable 
+-- It makes sure there are no duplicates.
 
+-- check data integrity constraint examples
+age int check( age > 21 ) 
+gender char( 1 ) check (gender in ('M','F','U'))
+email varchar(128)check(email like '%@%.%')
+phone char(14) check(phone like '(___) ___-____')
+
+-- To make sure only numbers can be added.
+phone char(14) check(phone like '([0-9][0-9][0-9]) [0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]')
+
+-- Altering Tables
+                                   
+ALTER TABLE <table>
+ADD <column> <data> {<attribute>};
+                                   
+
+alter table vendors 
+add vend_email char(225);
+
+alter table vendors 
+add vend_email char(225)
+                                   
+alter table
+alter <table> 
+                                   
+create view <view-name> as
+select ...;
